0f914b386b031021ed42874ce317898a3160f38c
Rahil Azhar <rahil.azhar10.com>
2024-08-04 18:47:01 +0500

    first commit

--- Next/src/middleware.js
+++ Next/src/middleware.js
@@ -3,12 +3,12 @@ import { NextResponse } from 'next/server';
 export function middleware(req) {
   const { nextUrl: url } = req;
 
-  // // Check if the URL contains a query string
-  // if (url.search) {
-  //   // Construct absolute URL for the redirect
-  //   const redirectUrl = new URL('/404', url.origin);
-  //   return NextResponse.redirect(redirectUrl);
-  // }
+  // Check if the URL contains a query string
+  if (url.search) {
+    // Construct absolute URL for the redirect
+    const redirectUrl = new URL('/404', url.origin);
+    return NextResponse.redirect(redirectUrl);
+  }
 
   return NextResponse.next();
 }
